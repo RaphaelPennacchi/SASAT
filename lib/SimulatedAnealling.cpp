e3183d5823f44f78a3ab7be83fabc8a4657b3f4a
Raphael Pennacchi <raphaelpennacchi1997@gmail.com>
2021-03-11 12:27:00 -0300

    Alterations on functions and updated SA, need to debug SA

--- lib/SimulatedAnealling.cpp
+++ lib/SimulatedAnealling.cpp
@@ -16,91 +16,60 @@ struct SimulatedAnealling {
     }
     return counter;
   }
-  // Pseudocode
-  // Let s = s0
-  // For k = 0 through kmax (exclusive):
-  //     T ← temperature( (k+1)/kmax )
-  //     Pick a random neighbour, snew ← neighbour(s)
-  //     If P(E(s), E(snew), T) ≥ random(0, 1):
-  //         s ← snew
-  // Output: the final state s
+
+  // TODO escolher a função
 
   double calculateTemperature(int actualIteration, int maxIteration, double initialTemp, double finalTemp) {
     return initialTemp - (double)actualIteration * ((initialTemp - finalTemp) / maxIteration);
   }
 
-  double calculateTemperature1(int actualIteration, int maxIteration, double initialTemp, double finalTemp) {
-    return (double)(initialTemp * pow((finalTemp / initialTemp), (actualIteration / maxIteration)));
-  }
-
   double calculateTemperature2(int actualIteration, int maxIteration, double initialTemp, double finalTemp) {
     double a = (double)((initialTemp - finalTemp) * (maxIteration + 1)) / (double)maxIteration;
     double b = (double)initialTemp - a;
-    return ((double)a / (double)(actualIteration + 1 )) + (double)b;
+    return ((double)a / (double)(actualIteration + 1)) + (double)b;
   }
 
-  double calculateTemperature3(int actualIteration, int maxIteration, double initialTemp, double finalTemp){
-    double a = (double)log(initialTemp - finalTemp) / log(maxIteration);
-    return (double)(initialTemp - pow((double)actualIteration, a));
-  }
-
-  double calculateTemperature4(int actualIteration, int maxIteration, double initialTemp, double finalTemp){
-    return ((initialTemp - finalTemp) / (1 + exp(3 * (1 - (maxIteration/2))))) + finalTemp;
-  }
-
-  double calculateTemperature5(int actualIteration, int maxIteration, double initialTemp, double finalTemp){
-    return ((double)1 / 2) * (initialTemp - finalTemp) * (1 + cos((actualIteration * M_PI)/ maxIteration)) + finalTemp;
-  }
-
-  double calculateTemperature6(int actualIteration, int maxIteration, double initialTemp, double finalTemp){
-    return ((double)1 / 2) * (initialTemp - finalTemp) * (1 - tanh(((double)(10*actualIteration)/maxIteration) - 5)) + finalTemp;
-  }
-
-  double calculateTemperature7(int actualIteration, int maxIteration, double initialTemp, double finalTemp){
-    return ((initialTemp - finalTemp) / cosh((double)(10*actualIteration)/ maxIteration)) + finalTemp;
-  }
-
-  double calculateTemperature8(int actualIteration, int maxIteration, double initialTemp, double finalTemp){
-    double a = (double)( 1 / maxIteration) * log((double)initialTemp/finalTemp);
-    return initialTemp * exp(-a * actualIteration);
-  }
-
-  double calculateTemperature9(int actualIteration, int maxIteration, double initialTemp, double finalTemp){
-    double a = ((double)1 / pow(maxIteration, 2)) * log((double)initialTemp / finalTemp);
-    return initialTemp * exp(-a * pow(actualIteration, 2));
-  }
-
-  pair <vector<bool>, int> run(Solution solution, vector <Clausule> clausule, int maxIterations, double initialTemp, double finalTemp) {
+  pair <vector<bool>, int> run(Solution solution, vector <Clausule> clausule, int maxIterations, double initialTemp, double finalTemp, int tempFunction) {
     // Set the initial solution
+    /*
+
+
+
+
+
+    */
     Solution fooSolution = solution;
     int sucess = 0, fooSucess = 0;
     int temperature = initialTemp;
-    int probability, cost;
-    for (int i = 0; i < maxIterations; i++) {
-      //Modifica a solucao em porcentagem
-      fooSolution.modify((solution.nVariable / 20) ? solution.nVariable/20 : 1);
-      //Verificar se a solução atual é melhor que a anterior
-      fooSucess = sucessAmount(fooSolution, clausule);
-      if (fooSucess > sucess) {
-        sucess = fooSucess;
-        solution = fooSolution;
-      }
-      else {
-        // Função para atualizar a temperatura
-        temperature = calculateTemperature(i, maxIterations, initialTemp, finalTemp);
-        // If para ver se aceita a nova solução ou não
-        probability = (double) rand() / RAND_MAX;
-        cost = exp((double)(fooSucess - sucess) / temperature);
-        
-        if (cost > probability) {
+    int probability, cost, counter = 0;
+    int i;
+    while (temperature > finalTemp) {
+      cout << counter++ << endl;
+      for (i = 0; i < maxIterations; i++) {
+        //Modifica a solucao em porcentagem
+        fooSolution.modify((solution.nVariable / 20) ? solution.nVariable / 20 : 1);
+        //Verificar se a solução atual é melhor que a anterior
+        fooSucess = sucessAmount(fooSolution, clausule);
+        if (fooSucess > sucess) {
           sucess = fooSucess;
           solution = fooSolution;
         }
+        else {
+          // Função para atualizar a temperatura
+          // If para ver se aceita a nova solução ou não
+          probability = (double)rand() / RAND_MAX;
+          cost = exp((double)-(fooSucess - sucess) / temperature);
+          if (cost > probability) {
+            sucess = fooSucess;
+            solution = fooSolution;
+          }
+        }
       }
+      if (tempFunction == 1) temperature = calculateTemperature2(i, maxIterations, initialTemp, finalTemp);
+      else temperature = calculateTemperature(i, maxIterations, initialTemp, finalTemp);
     }
     return make_pair(solution.solution, sucess);
   }
-
 };
 
 #endif // SIMULATEDANEALLING
--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include "lib/lib.hpp"
+#include <ctime>
+#include <sys/time.h>
+
+double wtime() {
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return t.tv_sec + (double) t.tv_usec / 1000000;
+}
 
 int main(int argc, char const* argv[]) {
   FileOperator file;
@@ -6,20 +14,26 @@ int main(int argc, char const* argv[]) {
   vector<Clausule> data = file.read(file.options[1]);
   Solution solution(file.nClause);
   solution.generate();
+  double initialTime, finalTime;
 
   SimulatedAnealling sa;
   RandomSearch rs;
   pair <vector<bool>, int> saResponse, rsResponse;
-  saResponse = sa.run(solution, data, 250000, 3.0, 0.01);
+  cout << "Argumento entrado: " << atoi(argv[1]) << endl;
+  initialTime = wtime();
+  saResponse = sa.run(solution, data, 250000, 3.0, 0.0001, atoi(argv[1]));
+  finalTime = wtime();
+  for (auto&& v : saResponse.first) {
+    cout << v;
+  }
+  cout << "\t" << saResponse.second << endl << "Time: " << finalTime - initialTime << endl;
+  initialTime = wtime();
   rsResponse = rs.run(solution, data, 250000);
-  for(auto&& v: saResponse.first){
+  finalTime = wtime();
+  for (auto&& v : rsResponse.first) {
     cout << v;
   }
-  cout << "\t" <<  saResponse.second << endl;
-  for(auto&& v: rsResponse.first){
-    cout << v;
-  }
-  cout << "\t" << rsResponse.second << endl;
+  cout << "\t" << rsResponse.second << endl << "Time: " << finalTime - initialTime << endl;
 
 
   return 0;
